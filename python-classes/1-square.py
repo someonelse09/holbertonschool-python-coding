569d76dd6159336d2022869bd09d752479125231
someonelse09 <burjuzada@gmail.com>
2025-06-25 14:16:44 +0000

    1-square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" This module defines a square class for geometric square representation """
+""" This module defines a square class
+for geometric square representation """
 
 
 class Square:
-    """ This class creates a square class with initializeed size inside of constructor """
+    """ This class creates a square class with
+    initializeed size inside of constructor """
 
     def __init__(self, size=0):
-        """ This method allows us to initialize an object based on the structure defined inside of this class """
+        """ This method allows us to initialize an object
+        based on the structure defined inside of this class """
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        
