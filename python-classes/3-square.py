12b94da0c53e3daf67ccaa4a48ae7577a32700e9
someonelse09 <burjuzada@gmail.com>
2025-06-25 15:14:44 +0000

    3-square && 3_main

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,21 +7,23 @@ class Square:
     """ A class that defines a Square by its size
     with private attribute control """
 
+    def __init__(self, size=0):
+        """ Initialize a new Square instance
+        with specified size """
+        self.__size = size
     @property
     def size(self):
+        """ Get the size of the square """
         return self.__size
-    @property
+    @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        """ Set the size of the with validation """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
         return self.__size
-    def __init__(self, size=0):
-        """ Initialize a new Square instance
-        with specified size """
-        self.__size = size
     def area(self):
+        """ Calculate and return the area of the square """
         return self.__size ** 2
-
