08f70a1e6b6bc4354fc1fbaa4f84abc2715e6a1a
someonelse09 <burjuzada@gmail.com>
2025-06-25 15:16:47 +0000

    3-square && 3_main

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,10 +11,12 @@ class Square:
         """ Initialize a new Square instance
         with specified size """
         self.__size = size
+
     @property
     def size(self):
         """ Get the size of the square """
         return self.__size
+
     @size.setter
     def size(self, value):
         """ Set the size of the with validation """
@@ -24,6 +26,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
         return self.__size
+
     def area(self):
         """ Calculate and return the area of the square """
         return self.__size ** 2
