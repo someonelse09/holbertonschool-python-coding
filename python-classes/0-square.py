de543d443a489c26b1755b3237333faa7a25f123
someonelse09 <burjuzada@gmail.com>
2025-06-25 13:41:16 +0000

    0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ This class defines a Square class
 for a geometric square representation """
+
+
 class Square:
     """ A class that defines a Square by its size
     with private attribute control """
+
     def __init__(self, size):
         """ Initialize a new Square instance
         with specified size """
